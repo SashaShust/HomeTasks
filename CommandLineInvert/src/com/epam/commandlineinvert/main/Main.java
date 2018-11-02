dbeaa4ef2179124a4d12af590210cc5381d32a0b
SashaShust <akuzmenko001@gmail.com>
2018-11-02 17:48:38 +0300

    gsdfgsdf

---
+++ CommandLineInvert/src/com/epam/commandlineinvert/main/Main.java
@@ -0,0 +1,9 @@
+package com.epam.commandlineinvert.main;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = args.length - 1; i >= 0; i--) {
+			System.out.println("Argument " + i + " = " + args[i]);
+		}
+	}
+}
